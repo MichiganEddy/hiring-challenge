9800e659c894c75f21d40c9b7091d132e7fae105
Chris Carlson <Chris@cta.cloud>
2017-01-15 11:34:07 -0500

    Minor Code Format Change.

--- typeScripts/Comment.ts
+++ typeScripts/Comment.ts
@@ -1,11 +1,6 @@
 // A Comment class, (called 'Commen' because there is a conflict with the string "Comment" somewhere else in the code, and I'm not sure where.).
 class Commen extends Note { 
   
-  
-  
-  
-  
-  
   // Getters and Setters for Note 
     // id
   getid(): string {
