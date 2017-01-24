bf583b80dd50d8014fd09236f4aa8dcc97f03eb2
Chris Carlson <Chris@cta.cloud>
2017-01-24 00:07:49 -0500

    In this branch, I'm going to eschew TypeScript in favor of using joi for model definitions, db models and collections generation, and data validation. I think there is better tooling available for porting joi schema to express models and mongoDB schema than for TypeScript; using it will allow for less code repition. It allows me to have a single definition for the model, in a joi schema, from which the database and application derive their model definitions.

--- public/javascripts/Note.js
+++
@@ -1,8 +0,0 @@
-// The note class is the base class for any kind of annotation, be it posting an
-// article, commenting on an article, or commenting on a comment. 
-var Note = (function () {
-    function Note(id) {
-        this.id = id;
-    }
-    return Note;
-}());
